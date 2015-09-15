4fddf5f219e157806665cb554ce9dc0499835eba
Jinyu Pan <swimablefish@gmail.com>
2015-09-15 19:44:56 +0800

    add cider

---
+++ .emacs.d/elpa/cider-spy-20150625.758/cider-spy-pkg.el
@@ -0,0 +1,1 @@
+(define-package "cider-spy" "20150625.758" "Spy on CIDER to get info" '((cider "0.5.0") (dash "2.5.0") (cl-lib "0.5")) :url "http://www.github.com/jonpither/cider-spy" :keywords '("languages" "clojure" "cider" "nrepl"))
